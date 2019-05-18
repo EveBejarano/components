8ec7ba5869147d18de5e10ce28d88cd64cd0f41e
EnzoEsper <enzoesper@hotmail.com>
2019-05-17 22:32:36 -0300

    wrap into the approval card the content of each comment

--- src/index.js
+++ src/index.js
@@ -2,28 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return(
     <div className="ui container comments">
-      <CommentDetail 
-        author="Sam" 
-        timeAgo="Today at 4:45PM" 
-        textComment="Nice blog fam" 
-        avatarImg={faker.image.avatar()}
-      />
-      <CommentDetail 
-        author="Alex" 
-        timeAgo="Today at 3:00PM" 
-        textComment="Nice blog dude" 
-        avatarImg={faker.image.avatar()} 
-      />
-      <CommentDetail 
-        author="Frank" 
-        timeAgo="Today at 2:00AM" 
-        textComment="Nice blog bro" 
-        avatarImg={faker.image.avatar()} 
-      />
+      <ApprovalCard>
+        <CommentDetail 
+          author="Sam" 
+          timeAgo="Today at 4:45PM" 
+          textComment="Nice blog fam" 
+          avatarImg={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Alex" 
+          timeAgo="Today at 3:00PM" 
+          textComment="Nice blog dude" 
+          avatarImg={faker.image.avatar()} 
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Frank" 
+          timeAgo="Today at 2:00AM" 
+          textComment="Nice blog bro" 
+          avatarImg={faker.image.avatar()} 
+        />
+      </ApprovalCard>
     </div>
   );
 };
